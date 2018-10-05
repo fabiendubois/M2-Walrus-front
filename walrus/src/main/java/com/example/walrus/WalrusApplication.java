075e147c661c6b3b213e3f698290cda40b300ac0
Fabien <fabien@MacBook-Pro-de-Fabien.local>
2018-10-05 14:21:14 +0200

    FDU : Spring Initializ

---
+++ walrus/src/main/java/com/example/walrus/WalrusApplication.java
@@ -0,0 +1,12 @@
+package com.example.walrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalrusApplication.class, args);
+	}
+}
