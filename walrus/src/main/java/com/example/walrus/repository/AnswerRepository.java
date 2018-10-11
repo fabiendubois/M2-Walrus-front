530008ee05e1c7944b0d47805459b4ea67b10dce
Fabien <fabien@MacBook-Pro-de-Fabien.local>
2018-10-11 08:12:38 +0200

    FDU : FindById Add FindAll

--- walrus/src/main/java/com/example/walrus/repository/AnswerRepository.java
+++ walrus/src/main/java/com/example/walrus/repository/AnswerRepository.java
@@ -1,35 +1,10 @@
 package com.example.walrus.repository;
 
-import org.springframework.data.jpa.repository.Query;
+import com.example.walrus.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Repository
-public class AnswerRepository {
-
-    /*
-    private static final HashMap<Integer,String> DATABASE = new HashMap<>();;
-
-    public AnswerRepository() {
-        DATABASE.put(1, "Reponse 1");
-        DATABASE.put(2, "Reponse 2");
-        DATABASE.put(3, "Reponse 3");
-        DATABASE.put(4, "Reponse 4");
-        DATABASE.put(5, "Reponse 5");
-    }
-
-    public List<String> getAll() {
-        return DATABASE.values().stream().collect(Collectors.toList());
-    }
-
-    public void create(String answer) {
-        DATABASE.put(DATABASE.keySet().size()+1, answer);
-    }
-    */
-
+public interface AnswerRepositoryItf extends JpaRepository<Answer, Integer> {
 
 }
